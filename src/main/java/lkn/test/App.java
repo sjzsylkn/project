5490bd876605df87994d34c6ce4b31c2d9a9c64b
LiKangning <123>
2016-08-09 19:49:09 +0800

    在idea中新建的工程,向git上提交

---
+++ src/main/java/lkn/test/App.java
@@ -0,0 +1,16 @@
+package lkn.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static final String SSSSSS = "23";
+
+    public static void main(String[] args) {
+//        Map<String, Integer> map = new HashMap<String, Integer>();
+        System.out.println("HELLO WORLD!");
+    }
+}
---
+++ src/main/java/lkn/test/Person.java
@@ -0,0 +1,22 @@
+package lkn.test;
+
+
+/**
+ *
+ * Created by LiKangning on 16/8/9.
+ */
+public class Person {
+
+
+    public static void main(String[] args) {
+        String s = "for aliance";
+        System.out.println(s);
+        System.out.println("");
+        System.out.println("hahhaahahahahahhah");
+        new Person().begin();
+    }
+
+    private void begin() {
+        System.out.println("为了联盟");
+    }
+}
