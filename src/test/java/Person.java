d47ceba075c6d467c45a816fa8b24cbf7d3e8f87
LiKangning <123>
2016-08-10 11:51:47 +0800

    新增加了测试类

---
+++ src/test/java/Person.java
@@ -0,0 +1,9 @@
+/**
+ * person类
+ * Created by LiKangning on 16/8/10.
+ */
+public class Person implements RunFastI {
+    public Integer runSpeed() {
+        return 28;
+    }
+}
---
+++ src/test/java/RunFastI.java
@@ -0,0 +1,6 @@
+/**
+ * Created by LiKangning on 16/8/10.
+ */
+public interface RunFastI {
+    public Integer runSpeed();
+}
